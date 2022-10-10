8cfe616adc5e799dcd9d13813127a5fb78f14454
artesgo <artesgo@gmail.com>
2022-10-10 11:29:22 -0400

    chore: keep scanner on screen

--- src/lib/components/input/BarCodeScanner.svelte
+++ src/lib/components/input/BarCodeScanner.svelte
@@ -2,17 +2,13 @@
 	import { createEventDispatcher, onMount } from 'svelte';
 	import { Html5QrcodeScanner } from 'html5-qrcode';
 	import Device from 'svelte-device-info';
-	import Button from '../cta/Button.svelte';
 
-	let isMobile = false;
 	let html5QrcodeScanner;
 	let scan = createEventDispatcher();
 	let error = createEventDispatcher();
-  let scanned = false;
 
 	function onScanSuccess(decodedText: string, decodedResult: any) {
 		scan(decodedText);
-    scanned = true;
 	}
 
 	function onScanFailure(err: any) {
@@ -24,10 +20,8 @@
 	});
 
   function bindScan() {
-    scanned = false;
     let scanTypes = [0,1];
 		if (Device.isMobile || Device.isPhone || Device.isTablet) {
-      isMobile = true;
       html5QrcodeScanner = new Html5QrcodeScanner(
         'reader',
         {
@@ -43,8 +37,4 @@
   }
 </script>
 
-{#if !scanned}
-  <div id="reader" />
-{:else}
-  <Button on:click={() => bindScan()}>Rescan</Button>
-{/if}
+<div id="reader" />
