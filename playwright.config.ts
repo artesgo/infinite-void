373a1ed0e4de366df8f78e29c00d1bbeff5ceddd
artesgo <artesgo@gmail.com>
2022-11-03 00:24:04 -0400

    chore: add projects config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,24 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices, PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
-	}
+	},
+	projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
 };
 
 export default config;
